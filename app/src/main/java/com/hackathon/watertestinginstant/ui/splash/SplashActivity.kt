c9413b59cb9e35aff3e4e5cad70e83dbce1dccc2
thaiminhpv <thaiminhpv@gmail.com>
2020-01-09 23:54:24 +0700

    flash screen redesign add app icon fix link resource bugs

--- app/src/main/java/com/hackathon/watertestinginstant/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/hackathon/watertestinginstant/ui/splash/SplashActivity.kt
@@ -1,13 +1,16 @@
 package com.hackathon.watertestinginstant.ui.splash
 
 import android.content.Context
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.hackathon.watertestinginstant.R
 import com.hackathon.watertestinginstant.appl.WaterTestingApplication
 import com.hackathon.watertestinginstant.ui.login.LoginActivity
 import com.hackathon.watertestinginstant.ui.main.MainActivity
+import kotlinx.android.synthetic.main.view_flash.*
 import kotlinx.coroutines.*
 
 
@@ -18,6 +21,15 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_flash)
+
+        //background animation
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        val animDrawable = flash_layout.background as AnimationDrawable
+        animDrawable.setEnterFadeDuration(10)
+        animDrawable.setExitFadeDuration(5000)
+        animDrawable.start()
+
         // go straight to main if a token is stored
         context = this
 
